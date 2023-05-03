115c749ec6d3921b80a8af0575e1c66fedd3e224
Almas Mirzandi <almasmirzandir@gmail.com>
2023-05-04 00:28:59 +0700

    discount route added

---
+++ pages/api/apply-discount.js
@@ -0,0 +1,26 @@
+import { PrismaClient } from '@prisma/client'
+import { getServerSession } from 'next-auth'
+
+export default async function handler(req, res) {
+    const { cartId } = req.body;
+  
+    try {
+      const cart = await prisma.cart.findUnique({ where: { id: cartId }, include: { item: true } });
+  
+      if (!cart) {
+        return res.status(404).json({ success: false, message: 'Cart not found' });
+      }
+  
+      const discountedTotalPrice = cart.totalPrice * 0.9; // apply 10% discount
+  
+      const updatedCart = await prisma.cart.update({
+        where: { id: cartId },
+        data: { totalPrice: discountedTotalPrice },
+      });
+  
+      res.status(200).json({ success: true, updatedCart });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ success: false, message: 'Internal server error' });
+    }
+  }
