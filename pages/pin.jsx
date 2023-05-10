61e8a3d1a13aa00a9a00280371c5464304972a7b
sickopam <sickopam@gmail.com>
2023-05-11 06:51:11 +0700

    pin ui incomplete

--- pages/pin.jsx
+++ pages/pin.jsx
@@ -26,9 +26,9 @@ const PinInput = () => {
         <div className='h-screen bg-[url(../images/bg-pay.png)]'>
             <div className='text-white font-bold text-center pt-[8rem] pb-[5rem]'>
                 <p className="text-[40px]">Rp118.560</p>
-                <p className='text-sm'>Enter pin to pay</p>
+                <p className='text-lg'>Enter pin to pay</p>
             </div>
-            <form onSubmit={handlePinSubmit} id='inputpin' className='w-full'>
+            {/* <form onSubmit={handlePinSubmit} id='inputpin' className='w-full'>
                 <input
                 required
                 type="password"
@@ -42,9 +42,24 @@ const PinInput = () => {
                         <h1>Submit</h1>
                     </div>
                 </Link>
-            </form>
+            </form> */}
+            <div id='pincontainer'>
+                {[0, 1, 2, 3].map(() => (
+                    <div id='pincircle'/>
+                ))}
+            </div>
+
+            <div id='pinum'>
+                {arr.map((i) => (
+                    <div key={i}>
+                        <h1 className='text-center'>{i}</h1>
+                    </div>
+                ))}
+            </div>
         </div>
     );
   };
   
   export default PinInput;
+  
+  let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
