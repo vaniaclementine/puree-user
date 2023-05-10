c49fed9c04d7300fb815bcc7d4cfa185ceeca9ba
sickopam <sickopam@gmail.com>
2023-05-11 06:31:32 +0700

    pin incomplete

--- pages/pin.jsx
+++ pages/pin.jsx
@@ -1,13 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Link from 'next/link';
 
-export default function Pin() {
-  return (
-    <div id='pin'>
-        <div className='text-white text-center mt-[8rem] font-bold'>
-            <p className='text-[40px]'>Rp118.560</p>
-            <p className='text-sm pt-2'>Enter your pin to pay</p>
+const PinInput = () => {
+    const [pin, setPin] = useState('');
+  
+    const handlePinChange = (event) => {
+      setPin(event.target.value);
+    };
+  
+    const handlePinSubmit = (event) => {
+      event.preventDefault();
+  
+      // Call your checking algorithm here to validate the entered PIN
+      const isValid = checkPin(pin);
+  
+      // Provide feedback to the user
+      if (isValid) {
+        alert('PIN is correct!');
+      } else {
+        alert('Invalid PIN. Please try again.');
+      }
+    };
+  
+    return (
+        <div className='h-screen bg-[url(../images/bg-pay.png)]'>
+            <div className='text-white font-bold text-center pt-[8rem] pb-[5rem]'>
+                <p className="text-[40px]">Rp118.560</p>
+                <p className='text-sm'>Enter pin to pay</p>
+            </div>
+            <form onSubmit={handlePinSubmit} id='inputpin' className='w-full'>
+                <input
+                required
+                type="password"
+                value={pin}
+                onChange={handlePinChange}
+                maxLength={4}
+                className='text-center' // Set the desired maximum PIN length
+                />
+                <Link href='/' className='flex justify-center'>
+                    <div id='submitbtn'>
+                        <h1>Submit</h1>
+                    </div>
+                </Link>
+            </form>
         </div>
-
-    </div>
-  )
-}
+    );
+  };
+  
+  export default PinInput;
