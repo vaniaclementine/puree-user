cd161c7d099ffac4940a366b05d43b52b86b8b93
almasmirzandir <almasmirzandir@gmail.com>
2023-05-17 19:33:37 +0700

    configure nextauth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -7,7 +7,24 @@ import { PrismaAdapter } from "@next-auth/prisma-adapter";
 const prisma = new PrismaClient()
 
 export const authOptions = {
-  // adapter: PrismaAdapter(prisma),
+  callbacks: {
+    session({ session, user }) {
+      if (session.user) {
+        session.user.id = user.id;
+      }
+      if (user.merchantId) {
+        session.user.merchantId = user.merchantId;
+      }
+      if (user.customerId) {
+        session.user.customerId = user.customerId;
+      }
+      if (user.driverId) {
+        session.user.driverId = user.driverId;
+      }
+      return session;
+    },
+  },
+  adapter: PrismaAdapter(prisma),
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
