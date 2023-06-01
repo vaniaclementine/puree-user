3c75b0b9d1a353c6b99c0d8c399b6137ed9c6e6f
Almas Mirzandi <almasmirzandir@gmail.com>
2023-06-01 17:46:23 +0700

    getItemsByMerchant fix

--- pages/api/shop_page/getItemsByMerchant.js
+++ pages/api/shop_page/getItemsByMerchant.js
@@ -4,23 +4,26 @@ import { getServerSession } from 'next-auth'
 const prisma = new PrismaClient()
 
 export default async function handler(req, res) {
-    const { name } = req.query
+    const { merchantName } = req.query
 
     try {
         const items = await prisma.user.findFirst({
             where: {
-                name: name
+                name: merchantName
             },
-            include: {
+            select: {
+                name: true,
                 Merchant: {
-                    include: {
-                        Item: true,
+                    select: {
+                        Item: true
                     }
                 }
             }
         });
-        const itemFromRelation = items?.Merchant.Item || [];
-        res.status(200).json({ success: true, items });
+
+        const name = items?.name || '';
+        const records = items?.Merchant.Item || [];
+        res.status(200).json({ success: true, name, records });
     } catch (error) {
         console.error(error);
         res.status(500).json({ success: false, message: 'Internal server error' });
